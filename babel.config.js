dfe8a71c836a9d22b4635eda92abed009027e741
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-10-12 17:26:00 -0600

    🐛 Refactor babel.config.js and package-lock.json to add babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin', 
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './',  // Puedes ajustar este alias según la estructura de tu proyecto
+          },
+        },
+      ],
+    ],
   };
 };
